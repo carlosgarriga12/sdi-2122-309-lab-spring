2ca609f86b1961a2b455fa5684a4518ff021d7db
carlosgarriga12 <carlosgarriga12@gmail.com>
2022-02-25 10:26:54 +0100

    SDI-2122-309-4.1-SpringBoot-Sesiones.

--- src/main/java/com/uniovi/notaneitor/controllers/MarksController.java
+++ src/main/java/com/uniovi/notaneitor/controllers/MarksController.java
@@ -10,6 +10,10 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
+import java.util.HashSet;
+import java.util.Set;
+
 @Controller
 public class MarksController {
 
@@ -22,8 +26,16 @@ public class MarksController {
     @Autowired
     private MarkValidator markValidator;
 
+    @Autowired
+    private HttpSession httpSession;
+
     @RequestMapping("/mark/list")
     public String getList(Model model) {
+        Set<Mark> consultedList= (Set<Mark>) httpSession.getAttribute("consultedList");
+        if ( consultedList == null ) {
+            consultedList = new HashSet<Mark>();
+        }
+        model.addAttribute("consultedList", consultedList);
         model.addAttribute("markList", marksService.getMarks());
         return "mark/list";
     }
